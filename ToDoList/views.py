77d1bcd4f4e075b467669d6e2d4892f8260eec8a
Kaito-shi <kaito.shimoura.apu@gmail.com>
2021-01-25 23:12:25 +0900

    仮トップページ作成
    Techpit2-3終わり

--- ToDoList/views.py
+++ ToDoList/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+def index(request):
+    #return HttpResponse("仮のトップページ")
+    return render(request, "ToDoList/index.html")
+
 # Create your views here.
